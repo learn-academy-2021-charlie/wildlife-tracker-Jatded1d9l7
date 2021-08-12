03bceb265646b71cc26b34ae765d1a388d257804
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-08-12 19:48:48 -0400

    destroying animals

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+Story: As the consumer of the API I can destroy an animal sighting in the database.
--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -10,6 +10,27 @@ class SightingsController < ApplicationController
         render json: sighting
     end
 
-    
+    def update
+        sighting = Sighting.find(params[:id])
+        sighting.update(sighting_params)
+        if sighting.valid?
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+end
 
+def destroy 
+    sighting = Sighting.find)params[:id])
+    if sighting.destroy
+        render json: sighting
+    else
+        render json: sighting.errors
+    end
+end
+
+private 
+    def sighting_params 
+    params.require(:sighting.permit(:animal_id, :date, :longitude, :latitude, :start_date, :end_date)
+    end
 end
